3a6c7e30309413c6f0708ec2869c6e6b8489d460
uriel-martinez1 <martinezu444@gmail.com>
2024-09-24 19:28:32 -0400

    Removing the RegisterForm component in the implementation of the RegisterPage.

--- payup-client/src/pages/RegisterPage.js
+++ payup-client/src/pages/RegisterPage.js
@@ -1,11 +1,10 @@
 import React from "react";
-import RegisterForm from "../components/RegisterForm";
 
 const RegisterPage = () => {
     return (
         <div>
             <h1>Register</h1>
-            <RegisterForm />
+            
         </div>
     );
 };
