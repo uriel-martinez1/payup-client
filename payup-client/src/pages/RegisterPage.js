a8d078ac32604d39e5dbd4fe50a0a06f8dd6724f
uriel-martinez1 <martinezu444@gmail.com>
2024-09-24 19:33:32 -0400

    Imported the RegisterForm component to the RegisterPage.

--- payup-client/src/pages/RegisterPage.js
+++ payup-client/src/pages/RegisterPage.js
@@ -1,10 +1,11 @@
 import React from "react";
+import RegisterForm from "../Components/RegisterForm";
 
 const RegisterPage = () => {
     return (
         <div>
             <h1>Register</h1>
-            
+            <RegisterForm />
         </div>
     );
 };
