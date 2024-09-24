83b467f5460a04b67f7703ae14bc14b626b36f37
uriel-martinez1 <martinezu444@gmail.com>
2024-09-24 18:43:03 -0400

    Created a RegisterPage component that lives in the pages folder in the src folder, this will be the 'view' that renders the RegisterForm component. Currently the RegisterForm imported in the RegisterPage.

--- payup-client/src/pages/RegisterPage.js
+++ payup-client/src/pages/RegisterPage.js
@@ -1,10 +1,12 @@
 import React from "react";
+import RegisterForm from "../Components/RegisterForm";
 
 export default function RegisterPage() {
     return (
         <>
             <h1>Register</h1>
             <p>This is the register page</p>
+            <RegisterForm />
         </>
     );
 }
