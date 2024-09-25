7dc0a1ec9b040593bbae77f25ae10824e49b2da2
uriel-martinez1 <martinezu444@gmail.com>
2024-09-25 16:53:14 -0400

    Updated the app.js file to include the option to Logout when a user has signed in.

--- payup-client/src/App.js
+++ payup-client/src/App.js
@@ -3,28 +3,45 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
+import AuthContext, { AuthProvider } from './store/AuthContext';
 
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <div className="nav">
-          <h1>PayUp App</h1>
-          <Link to='/'>Home</Link>
-          <span>
-            &nbsp;|&nbsp;
-            <Link to='/register'>Register</Link>
-          </span>
-        </div>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <div className="nav">
+            <h1>PayUp App</h1>
+            <Link to='/'>Home</Link>
+            <span>&nbsp;|&nbsp;</span>
+            <AuthContext.Consumer>
+              {({authState, logout}) => {
+                authState.token ? (
+                  <>
+                    <Link to='/home'>Home</Link>
+                    &nbsp;|&nbsp;
+                    <button onClick={logout}>Logout</button>
+                  </>
+                ) : (
+                  <>
+                    <Link to='/register'>Register</Link>
+                    &nbsp;|&nbsp;
+                    <button onClick={logout}>Login</button>
+                  </>
+                )
+              }}    
+            </AuthContext.Consumer>
+          </div>
 
-        <Routes>
-          <Route path='/' element={<LoginPage />} />
-          <Route path='/register' element={<RegisterPage />} />
-          <Route path='/home/:userId' element={<HomePage />} />
-        </Routes>
-      </div>
-    </Router>
+          <Routes>
+            <Route path='/' element={<LoginPage />} />
+            <Route path='/register' element={<RegisterPage />} />
+            <Route path='/home/:userId' element={<HomePage />} />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
