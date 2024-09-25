479d4f642f1844e9848561f02bb9e7091e31c915
uriel-martinez1 <martinezu444@gmail.com>
2024-09-25 13:50:17 -0400

    Created a HomePage file to eventually hold more components.

---
+++ payup-client/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+// this is where we will eventually load in the different components
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to the main page!</h1>
+        </div>
+    );
+};
+
+export default HomePage;
