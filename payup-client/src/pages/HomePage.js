a89b2c83a5fbc9750fac080b62fe78a11105032a
uriel-martinez1 <martinezu444@gmail.com>
2024-09-26 16:41:21 -0400

    Added the UserBalance component in the HomePage component.

--- payup-client/src/pages/HomePage.js
+++ payup-client/src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 // this is where we will eventually load in the different components
+import UserBalance from "../components/UserBalance";
 
 const HomePage = () => {
     return (
         <div>
             <h1>Welcome to the main page!</h1>
+            <UserBalance />
         </div>
     );
 };
