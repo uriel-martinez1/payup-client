17bc9162a2d1f136b33848328016fa91b2fa2ab3
uriel-martinez1 <martinezu444@gmail.com>
2024-09-26 16:55:05 -0400

    Adjusted the homepage to include the username in the main page. Had to add the AuthContext to the HomePage component to be able to grab the user information in the datastore. This is stored when the user has logged in.

--- payup-client/src/pages/HomePage.js
+++ payup-client/src/pages/HomePage.js
@@ -1,11 +1,12 @@
-import React from "react";
-// this is where we will eventually load in the different components
+import React, {useContext} from "react";
 import UserBalance from "../components/UserBalance";
+import AuthContext from "../store/AuthContext";
 
 const HomePage = () => {
+    const {authState} = useContext(AuthContext);
     return (
         <div>
-            <h1>Welcome to the main page!</h1>
+            <h1>Welcome to the main page, {authState.user.username}!</h1> {/** Eventually we will need to adjust the return object to include user info like first, last name*/}
             <UserBalance />
         </div>
     );
