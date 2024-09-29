9b926a0632fd96f79874bae140dd555e262bb99a
uriel-martinez1 <martinezu444@gmail.com>
2024-09-28 23:01:25 -0400

    Created a createTransformPage component that will render the TranformForm component.

---
+++ payup-client/src/pages/CreateTransferPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TransferForm from "../components/TransferForm";
+
+const CreateTransferPage = () => {
+    return (
+        <>
+        <TransferForm />
+        </>
+    )
+};
+
+export default CreateTransferPage;
