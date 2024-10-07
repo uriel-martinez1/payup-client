1f717f314c88bab3ca4d98ef29aef626b5811fbe
uriel-martinez1 <martinezu444@gmail.com>
2024-10-07 18:38:04 -0400

    Created the RequestUpdateStatusPage component for eventual implementation of the update form.

---
+++ payup-client/src/pages/RequestUpdateStatusPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
+
+const RequestUpdateStatusPage = () => {
+    const location = useLocation();
+    const { transfer } = location.state || {};
+    // we will need to make a call to the backend for update the status of the transfer
+
+    if (!transfer) {
+        return <p>No transfer details available.</p>
+    }
+
+    return (
+        <div>
+            This is the request update status page for {transfer.transferId}
+        </div>
+    )
+}
+export default RequestUpdateStatusPage;
