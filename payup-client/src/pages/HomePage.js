7a7c92bdeece18d45fb0ede131fd2c306635fc6a
uriel-martinez1 <martinezu444@gmail.com>
2024-10-09 21:32:13 -0400

    Updated the HomePage component so it uses the TransferContext.

--- payup-client/src/pages/HomePage.js
+++ payup-client/src/pages/HomePage.js
@@ -1,49 +1,52 @@
 import React, { useContext, useEffect, useState } from "react";
 import UserBalance from "../components/UserBalance";
-import AuthContext from "../store/AuthContext";
 import TransferCards from "../components/TransferCards";
 import SubmitButton from "../components/SubmitButton";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
 
-const transfersApiEndpoint = process.env.REACT_APP_PAYUP_SERVER_BASEURL + '/api/account/transfers';
+import AuthContext from "../store/AuthContext";
+import { TransferContext } from "../store/TransferContext";
+
+//const transfersApiEndpoint = process.env.REACT_APP_PAYUP_SERVER_BASEURL + '/api/account/transfers';
 
 const HomePage = () => {
     const { authState } = useContext(AuthContext);
-    // const [transfers, setTransfers] = useState([]); // what to create an array that will holds of the transfer objects from the response
-    const [requests, setRequests] = useState([]); // this will hold all transfer requests sent to the user
-    const [otherTransfers, setOtherTransfers] = useState([]); // this will holds the rest of the transfers
-    const [error, setError] = useState(null);
+    const {transfers, error} = useContext(TransferContext); // what to create an array that will holds of the transfer objects from the response
+    //const [requests, setRequests] = useState([]); // this will hold all transfer requests sent to the user
+    //const [otherTransfers, setOtherTransfers] = useState([]); // this will holds the rest of the transfers
+    //const [error, setError] = useState(null);
     const navigate = useNavigate(); // useNavigate hook for navigating to the transferForm
 
-    useEffect(() => {
-        //function to fetch transfer objects
-        const fetchTransfers = async () => {
-            try {
-                const response = await axios.get(transfersApiEndpoint, {
-                    headers: {
-                        'Authorization': `Bearer ${authState.token}`
-                    }
-                });
-                const transfersData = Array.isArray(response.data) ? response.data : []; // once we get a response, place responses in the array transfer
-                console.log(response.data);
+    const requests = transfers.filter((transfer) => transfer.transferType === 'Request' && transfer.transferStatus === 'Pending' && transfer.userFrom.id === authState.user.userId);
+    const otherTransfers = transfers.filter((transfer) => !(transfer.transferType === 'Request' && transfer.transferStatus === 'Pending' && transfer.userFrom.id === authState.user.userId));
+    // useEffect(() => {
+    //     //function to fetch transfer objects
+    //     const fetchTransfers = async () => {
+    //         try {
+    //             const response = await axios.get(transfersApiEndpoint, {
+    //                 headers: {
+    //                     'Authorization': `Bearer ${authState.token}`
+    //                 }
+    //             });
+    //             const transfersData = Array.isArray(response.data) ? response.data : []; // once we get a response, place responses in the array transfer
+    //             console.log(response.data);
 
-                const requestsData = transfersData.filter((transfer) => transfer.transferType === 'Request' && transfer.transferStatus === 'Pending' && transfer.userFrom.id === authState.user.userId); // filter the requests and add the request objects into a seperate array
-                setRequests(requestsData); // add the filtered array to the requests array
-                console.log(requestsData);
+    //             const requestsData = transfersData.filter((transfer) => transfer.transferType === 'Request' && transfer.transferStatus === 'Pending' && transfer.userFrom.id === authState.user.userId); // filter the requests and add the request objects into a seperate array
+    //             setRequests(requestsData); // add the filtered array to the requests array
+    //             console.log(requestsData);
 
-                const otherTransferData = transfersData.filter((transfer) => !(transfer.transferType === 'Request' && transfer.transferStatus === 'Pending' && transfer.userFrom.id === authState.user.userId));
-                setOtherTransfers(otherTransferData);
-                console.log(otherTransferData);
+    //             const otherTransferData = transfersData.filter((transfer) => !(transfer.transferType === 'Request' && transfer.transferStatus === 'Pending' && transfer.userFrom.id === authState.user.userId));
+    //             setOtherTransfers(otherTransferData);
+    //             console.log(otherTransferData);
 
-            } catch (err) {
-                setError('Error fetching the transfers.');
-            }
-        };
+    //         } catch (err) {
+    //             setError('Error fetching the transfers.');
+    //         }
+    //     };
 
-        fetchTransfers(); // this initiates the call to get transfers
+    //     fetchTransfers(); // this initiates the call to get transfers
 
-    }, [authState.token,authState.user.userId]); // we need this to ensure the useEffect hook only runs if the token changes in authContext
+    // }, [authState.token,authState.user.userId]); // we need this to ensure the useEffect hook only runs if the token changes in authContext
 
     // creating state for navigation tabs
     const [activeTab, setActiveTab] = useState('all');  // requests or all
